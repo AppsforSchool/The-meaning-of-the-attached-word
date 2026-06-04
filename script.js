1b38ecadf62ecccaac179ab572e1bd27ba3d3705
AppsforSchool <sugaku496@icloud.com>
2026-06-04 12:14:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,8 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 //qtype buttons
-let questionSelectionState = "auxiliaryVerb";
+let qtypeSelectionState = "auxiliaryVerb";
+let questionSelectionState = "word";
 let answerSelectionState = "meaning";
 let qtypeButtons;
 document.addEventListener('DOMContentLoaded', () => {
@@ -62,6 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // 選択状態をリセット
       qtypeButtons.forEach((btn) => btn.classList.remove("active"));
       button.classList.add("active");
+      qtypeSelectionState = button.dataset.qtype;
       questionSelectionState = button.dataset.question;
       answerSelectionState = button.dataset.answer;
     });
@@ -95,8 +97,9 @@ document.addEventListener('DOMContentLoaded', () => {
     startButton.textContent = "処理中...";
     const params = new URLSearchParams();
     params.append("count", countSelectionState);
+    params.appednd("qtype", qtypeSelectionState);
     params.append("question", questionSelectionState);
-    parms.append("answer", answerSelectionState);
+    params.append("answer", answerSelectionState);
     if (typeSelectionState === "card") {
       //console.log(`card.html?${params.toString()}`);
       window.location.href = `card.html?${params.toString()}`;
